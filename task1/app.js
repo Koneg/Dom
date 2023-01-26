07fd6a930f75ee061bbae291e31ac49dd5215d2e
Koneg <serogin@gmail.com>
2023-01-26 19:11:49 +0300

    start events

--- task1/app.js
+++
@@ -1,7 +0,0 @@
-let but = document.getElementById("but");
-let h1 = document.getElementById("h1");
-if (but) {
-  but.addEventListener("click", () => {
-    h1.innerText = "menyaetsa";
-  });
-}
--- task3/app.js
+++
@@ -1,7 +0,0 @@
-let but = document.getElementById("but");
-let table = document.getElementById("table");
-if (but) {
-  but.addEventListener("click", () => {
-    table.innerHTML += "<tr><td>January</td><td>$100</td></tr>";
-  });
-}
